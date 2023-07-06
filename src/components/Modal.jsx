aad909aa74e479e4b9219f4ec70ecbde41d40270
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-06 22:14:38 +0900

    before merge

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 function Modal(props) {
+  const onClickModalHandler = (e) => {};
   return (
     <StyledModal>
       <h2>Modal</h2>
       <button>Opne Modal!!</button>
+      <div className="modal"></div>
     </StyledModal>
   );
 }
@@ -16,4 +18,15 @@ const StyledModal = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  .modal {
+    width: 500px;
+    height: 500px;
+    position: absolute;
+    border-radius: 10px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #f2f5ff;
+  }
 `;
