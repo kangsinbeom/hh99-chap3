249f42fec1420ced2465c42e4d2cdb9a2937e94a
Haegnim <haegnim@gmail.com>
2023-07-06 16:33:35 +0900

    test

---
+++ src/assets/icons.js
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+export const Heart = () => {
+    return <FontAwesomeIcon icon={faHeart} />;
+};
