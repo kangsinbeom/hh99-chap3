750f93591044f2336f133ca70144d8a6df9e0a85
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-10 14:38:09 +0900

    last commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,47 +1,75 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import styled from 'styled-components';
-import '../assets/styles/modal.css';
-import Icon from '../assets/icons/icons';
-import Button from './Button';
-function Modal(props) {
-    const [modalChecked, setModalChecked] = useState(false);
+import React from "react";
+import ReactDOM from "react-dom";
+import { styled, css } from "styled-components";
+import "../assets/styles/modal.css";
+import Icon from "../assets/icons/icons";
+import Button from "./Button";
 
-    const onClickModalHandler = () => {
-        let checked = !modalChecked;
-        setModalChecked(checked);
-    };
+export const ModalOne = ({ onClick, modalChecked }) => {
+  return (
+    <StyledModal>
+      <Button.Primary width="250px" onClick={onClick}>
+        <Icon.Heart />
+        open modal
+        <Icon.Heart />
+      </Button.Primary>
+      {modalChecked
+        ? ReactDOM.createPortal(
+            <>
+              <div className="modal-background" onClick={onClick}></div>
+              <div className="modal">
+                <Button.Icon onClick={onClick}>
+                  <Icon.Xmark />
+                </Button.Icon>
+                <p>Modal version1</p>
+              </div>
+            </>,
+            document.getElementById("portal")
+          )
+        : null}
+    </StyledModal>
+  );
+};
 
-    return (
-        <StyledModal>
-            <Button.Primary width="250px" onClick={onClickModalHandler}>
-                <Icon.Heart />
-                open modal
-                <Icon.Heart />
-            </Button.Primary>
-            {modalChecked
-                ? ReactDOM.createPortal(
-                      <>
-                          <div className="modal-background" onClick={onClickModalHandler}></div>
-                          <div className="modal">
-                              <Button.Icon onClick={onClickModalHandler}>
-                                  <Icon.Xmark />
-                              </Button.Icon>
-                              <p>Modal version1</p>
-                          </div>
-                      </>,
-                      document.getElementById('portal')
-                  )
-                : null}
-        </StyledModal>
-    );
-}
-
-export default Modal;
+export const ModalTwo = ({ onClick, modalChecked }) => {
+  return (
+    <StyledModal>
+      <Button.Secondary onClick={onClick}>
+        <Icon.Heart />
+        open modalTwo
+      </Button.Secondary>
+      {modalChecked
+        ? ReactDOM.createPortal(
+            <>
+              <div className="modal-background"></div>
+              <div className="modal">
+                <p>Modal version2</p>
+                <Button.Primary width="100px" $center="center">
+                  {" "}
+                  check
+                </Button.Primary>
+                <Button.Secondary
+                  width="100px"
+                  $center="center"
+                  onClick={onClick}
+                >
+                  {" "}
+                  close
+                </Button.Secondary>
+              </div>
+            </>,
+            document.getElementById("portal")
+          )
+        : null}
+    </StyledModal>
+  );
+};
 
 const StyledModal = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
+
+// 공통부분을 빼보자
